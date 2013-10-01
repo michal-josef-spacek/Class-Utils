f410be3d0cdaef5597a5b49bd643811693828f74
Michal Špaček <tupinek@gmail.com>
2013-10-01 07:44:01 +0200

    Prepare for 0.05 version.

--- Utils.pm
+++ Utils.pm
@@ -13,7 +13,7 @@ use Readonly;
 Readonly::Array our @EXPORT_OK => qw(set_params);
 
 # Version.
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Set parameters to user values.
 sub set_params {
@@ -126,6 +126,6 @@ BSD license.
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
--- t/Class-Utils/01-version.t
+++ t/Class-Utils/01-version.t
@@ -8,4 +8,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($Class::Utils::VERSION, 0.04, 'Version.');
+is($Class::Utils::VERSION, 0.05, 'Version.');
