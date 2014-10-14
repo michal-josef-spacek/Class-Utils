925069a681c4eb2313e78ab829d46755a959e3c0
Michal Špaček <tupinek@gmail.com>
2014-10-14 12:25:50 +0200

    Improve EXAMPLE5 section in doc.

--- examples/ex5.pl
+++ examples/ex5.pl
@@ -7,20 +7,15 @@ use warnings;
 # Modules.
 use Class::Utils qw(split_params);
 
-# Hash reference with default parameters.
-my $self = {
-       'foo' => undef,
-};
-
 # Example parameters.
 my @params = qw(foo bar bad value);
 
 # Set bad params.
-my ($object_params_ar, $other_params_ar) = split_params(['foo'], @params);
+my ($main_params_ar, $other_params_ar) = split_params(['foo'], @params);
 
 # Print out.
 print "Main params:\n";
-print "* ".(join ': ', @{$object_params_ar});
+print "* ".(join ': ', @{$main_params_ar});
 print "\n";
 print "Other params:\n";
 print "* ".(join ': ', @{$other_params_ar});
