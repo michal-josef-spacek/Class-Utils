0534cceb695958ec3eaa238abd5f4b7af74c6582
Michal Josef Špaček <tupinek@gmail.com>
2020-08-06 00:26:56 +0200

    Improve LICENSE AND COPYRIGHT section in doc

--- Utils.pm
+++ Utils.pm
@@ -276,8 +276,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © Michal Josef Špaček 2011-2020
- BSD 2-Clause License
+© Michal Josef Špaček 2011-2020
+
+BSD 2-Clause License
 
 =head1 VERSION
 
