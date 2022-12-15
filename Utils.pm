dbfcc27026946f6f4b968523e653f6fbe846900c
Michal Josef Špaček <tupinek@gmail.com>
2022-12-15 16:07:05 +0100

    Prepare for 0.14 version

--- Utils.pm
+++ Utils.pm
@@ -13,7 +13,7 @@ Readonly::Array our @EXPORT_OK => qw(set_params set_params_pub set_split_params
 	split_params);
 
 # Version.
-our $VERSION = 0.13;
+our $VERSION = 0.14;
 
 # Set parameters to user values.
 sub set_params {
@@ -329,6 +329,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.13
+0.14
 
 =cut
